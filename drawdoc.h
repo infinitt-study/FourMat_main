7184a0742c7b192975a50d83dc0e6667c931f116
Hyunmin Kim <69665366+lehmon412@users.noreply.github.com>
2022-07-07 10:30:27 +0900

    현민 작업 1-3 하위 브랜치 생성

--- drawdoc.h
+++ drawdoc.h
@@ -117,7 +117,7 @@ public:
 	afx_msg void OnFeatureextractionReducenoise();
 	afx_msg void OnFeatureextractionSharpening();
 	afx_msg void OnFilteringBrightness();
-	afx_msg void OnFilteringContrast();
+	//afx_msg void OnFilteringContrast();
 	afx_msg void OnFilteringRemovenoise();
 	afx_msg void OnFilteringTograyscale();
 	afx_msg void OnFilteringHistogram();
